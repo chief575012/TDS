09e8ff1a9e62ad78394cb08d971623af7547ff07
chief575012 <chief5750@strayhood.org>
2026-06-11 07:43:07 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local Game = 5591597781
+local Lobby = 0
 function claimRewards()
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("DailySpin"):WaitForChild("RF:RedeemReward"):InvokeServer()
 end
@@ -5,4 +7,16 @@ function JoinElevator(v)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("RF:Enter"):InvokeServer(v)
   task.wait(0.05)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("RF:SetSize"):InvokeServer(1)
+end
+function TDS:Place(Tower,)
+
+end
+function CheckMap(Maps)
+if game.PlaceId == Game then 
+repeat task.wait() until workspace:GetAttribute("Map")
+    if workspace:GetAttribute("Map") == Maps then 
+      return true
+    else
+      return false
+  end
 end
