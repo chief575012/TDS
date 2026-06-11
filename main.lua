87ffc4ef8db849861b28126047e6de48a3663eae
chief575012 <chief5750@strayhood.org>
2026-06-11 07:39:20 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
 function claimRewards()
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("DailySpin"):WaitForChild("RF:RedeemReward"):InvokeServer()
 end
+function JoinElevator(v)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("RF:Enter"):InvokeServer(v)
+end
