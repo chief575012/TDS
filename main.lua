4e4bd46855467861d6559d7943f11c3f1b78c85f
chief575012 <chief5750@strayhood.org>
2026-06-11 09:54:13 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,9 +8,9 @@ game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Eleva
   task.wait(0.05)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("RF:SetSize"):InvokeServer(1)
 end
-function TDS:Place(Tower,)
-
-end
+function Ready()
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(	"Difficulty","Ready")
+  end
 function CheckMap(Maps)
 if game.PlaceId == Game then 
 repeat task.wait() until workspace:GetAttribute("Map")
@@ -20,3 +20,36 @@ repeat task.wait() until workspace:GetAttribute("Map")
       return false
   end
 end
+function PickMode(Mode)
+
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer("Difficulty",
+	"Vote",Mode)
+  end
+
+  function TDS:Place()
+  local args = {
+	"Troops",
+	"Place",
+	{
+		Rotation = CFrame.new(0, 0, 0, 1, -0, 0, 0, 1, -0, 0, 0, 1),
+		Position = vector.create(5.346579551696777, 0.24998024106025696, 26.561914443969727)
+	},
+	"Scout"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
+  end
+  function TDS:Loadout(List)
+
+  end
+  function TDS:Upgrade(Tower,Time,Path)
+    local Time = game:GetService("ReplicatedStorage").State.Timer.Time 
+    repeat task.wait() until Time.Value < Time 
+    repeat  local Remote = game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(	"Troops",
+	"Upgrade",
+	"Set",
+	{
+		Troop = workspace:WaitForChild("Towers"):WaitForChild(Tower),
+		Path = 1
+	})
+    until Remote == true
+  end
